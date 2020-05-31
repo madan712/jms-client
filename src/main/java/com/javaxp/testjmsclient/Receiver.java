d24331fd080330f60c4e595c3df206728ccdd5fc
Madan <madan712@gmail.com>
2020-05-31 11:03:42 +0100

    Test update

--- src/main/java/com/javaxp/testjmsclient/Receiver.java
+++ src/main/java/com/javaxp/testjmsclient/Receiver.java
@@ -1,4 +1,4 @@
-package com.example.testjmsclient;
+package com.javaxp.testjmsclient;
 
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
--- src/main/java/com/javaxp/testjmsclient/TestJmsClientApplication.java
+++ src/main/java/com/javaxp/testjmsclient/TestJmsClientApplication.java
@@ -1,4 +1,4 @@
-package com.example.testjmsclient;
+package com.javaxp.testjmsclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
